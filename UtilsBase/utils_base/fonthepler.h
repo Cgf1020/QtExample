ba0b7931c7f2765198ba1e0879b1505a81fbb5c3
CGF <cgf_1020@163.com>
2023-09-22 14:31:00 +0800

    增加字体库的学习博客

--- UtilsBase/utils_base/fonthepler.h
+++ UtilsBase/utils_base/fonthepler.h
@@ -2,6 +2,15 @@
 #define FONTHEPLER_H
 
 #include <QObject>
+/*Qt 对字体的使用
+ * 1. qt 加载字体库
+ * 2. qss中使用第三方的字体库
+ * https://blog.csdn.net/weixin_43165135/article/details/129929272
+ *
+*/
+
+
+
 
 class FontHepler : public QObject
 {
@@ -19,9 +28,6 @@ public:
 
 
 private:
-
-
-
     //默认构造函数,传入字体文件+字体名称
     explicit FontHepler(QObject *parent = 0);
 
